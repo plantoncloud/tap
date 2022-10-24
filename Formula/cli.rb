17fdc865144f7af33b8fc59a4db46d3ac48e505c
swarupdonepudi <swarup.donepudi@gmail.com>
2022-10-24 03:35:58 -0700

    updated planton-cli version to v5 which has several bug fixes and improvements for build engine

--- Formula/cli.rb
+++ Formula/cli.rb
@@ -1,5 +1,5 @@
 class Cli < Formula
-  version "v0.0.4"
+  version "v0.0.5"
   desc "planton-cli: cli interface to interact with planton cloud service"
   homepage "https://planton.cloud"
   os_arch = `arch`
