eafdc0b5a8fc12727548549b6e0959907d96b711
swarupdonepudi <swarup.donepudi@gmail.com>
2022-10-26 23:18:11 -0700

    updated cli version to v27 that has single domain external and internal support

--- Formula/cli.rb
+++ Formula/cli.rb
@@ -1,5 +1,5 @@
 class Cli < Formula
-  version "v0.0.6"
+  version "v0.0.7"
   desc "planton-cli: cli interface to interact with planton cloud service"
   homepage "https://planton.cloud"
   os_arch = `arch`
