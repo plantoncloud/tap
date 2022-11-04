874962aef44aa4dfd97f3737cf18368d85e8dad2
Swarup Donepudi <swarup@leftbin.com>
2022-11-04 12:47:19 -0700

    updated planton-cli version to v0.0.12 that uses latest mactl dep

--- Formula/cli.rb
+++ Formula/cli.rb
@@ -1,5 +1,5 @@
 class Cli < Formula
-  version "v0.0.11"
+  version "v0.0.12"
   desc "planton-cli: cli interface to interact with planton cloud service"
   homepage "https://planton.cloud"
   os_arch = `arch`
