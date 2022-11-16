05ac27f231781a35b28e20518d29e5c2aebc44aa
swarupdonepudi <swarup.donepudi@gmail.com>
2022-11-16 00:32:09 -0800

    updated mactl to v0.0.7

--- Formula/mactl.rb
+++ Formula/mactl.rb
@@ -1,5 +1,5 @@
 class Mactl < Formula
-  version "v0.0.6"
+  version "v0.0.7"
   desc "mactl: cli to quickly bootstrap macbooks."
   homepage "https://github.com/plantoncloud/mactl"
   os_arch = `arch`
