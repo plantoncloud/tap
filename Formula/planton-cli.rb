3b4dbecaa207dbe6433c0a0b48da85759494a151
swarupdonepudi <swarup.donepudi@gmail.com>
2022-11-26 22:50:57 -0800

    upgraded planton cli to v0.0.23 which has fixes for build engine java and proto projects

--- Formula/planton-cli.rb
+++ Formula/planton-cli.rb
@@ -1,5 +1,5 @@
 class PlantonCli < Formula
-  version "v0.0.22"
+  version "v0.0.23"
   desc "planton-cli: cli interface to interact with planton cloud service"
   homepage "https://planton.cloud"
   os_arch = `arch`
