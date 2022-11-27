432f29d3a2950493af1960204a21fd5e99763a80
swarupdonepudi <swarup.donepudi@gmail.com>
2022-11-26 21:26:15 -0800

    upgrdaed planton-cli to v0.0.21 with has build engine fixes for remote-debug

--- Formula/planton-cli.rb
+++ Formula/planton-cli.rb
@@ -1,5 +1,5 @@
 class PlantonCli < Formula
-  version "v0.0.21"
+  version "v0.0.22"
   desc "planton-cli: cli interface to interact with planton cloud service"
   homepage "https://planton.cloud"
   os_arch = `arch`
