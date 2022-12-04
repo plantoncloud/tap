dd61e2803be9fce0c9f0d4a2a57fe636b35eda4a
Swarup Donepudi <swarup@Swarups-Mac-Studio.local>
2022-12-04 00:18:53 -0800

    upgraded planton-cli to v0.0.26 that has proto upgrades made for default endpoint support

--- Formula/planton-cli.rb
+++ Formula/planton-cli.rb
@@ -1,5 +1,5 @@
 class PlantonCli < Formula
-  version "v0.0.25"
+  version "v0.0.26"
   desc "planton-cli: cli interface to interact with planton cloud service"
   homepage "https://planton.cloud"
   os_arch = `arch`
