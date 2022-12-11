cd5272ec108bf3d80611636a9943d04dbeeb8659
swarupdonepudi <swarup.donepudi@gmail.com>
2022-12-11 02:59:01 -0800

    updated planton-cli to v0.0.27 which has working version of microservice log tailing

--- Formula/planton-cli.rb
+++ Formula/planton-cli.rb
@@ -1,5 +1,5 @@
 class PlantonCli < Formula
-  version "v0.0.26"
+  version "v0.0.27"
   desc "planton-cli: cli interface to interact with planton cloud service"
   homepage "https://planton.cloud"
   os_arch = `arch`
