a7fcf8825c7edd61e49ffd9d9ac3321780a7ccdf
Swarup Donepudi <swarup@Swarups-Mac-Studio.local>
2022-12-14 13:56:21 -0800

    planton-cli v28 which has fix for code project lookup by code-server

--- Formula/planton-cli.rb
+++ Formula/planton-cli.rb
@@ -1,5 +1,5 @@
 class PlantonCli < Formula
-  version "v0.0.27"
+  version "v0.0.28"
   desc "planton-cli: cli interface to interact with planton cloud service"
   homepage "https://planton.cloud"
   os_arch = `arch`
