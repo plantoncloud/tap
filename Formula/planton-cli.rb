de5c6ffc5a0a8f9a17609baf6cecf8c5565a860b
swarupdonepudi <swarup.donepudi@gmail.com>
2023-01-14 08:00:47 -0800

    planton-cli v35

--- Formula/planton-cli.rb
+++ Formula/planton-cli.rb
@@ -1,5 +1,5 @@
 class PlantonCli < Formula
-  version "v0.0.34"
+  version "v0.0.35"
   desc "planton-cli: cli interface to interact with planton cloud service"
   homepage "https://planton.cloud"
   os_arch = `arch`
