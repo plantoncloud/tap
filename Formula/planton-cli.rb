4de3d87f063c432ba5a78f267c411b7fc42a87f7
Swarup Donepudi <swarup@Swarups-Mac-Studio.local>
2023-04-30 13:00:11 -0700

    upgraded planton-cli version to v39 that supports solrcloud resources and proto upgrades for resource type in commons

--- Formula/planton-cli.rb
+++ Formula/planton-cli.rb
@@ -1,5 +1,5 @@
 class PlantonCli < Formula
-  version "v0.0.35"
+  version "v0.0.39"
   desc "planton-cli: cli interface to interact with planton cloud service"
   homepage "https://planton.cloud"
   os_arch = `arch`
