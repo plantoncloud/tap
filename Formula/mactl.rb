c3e66bc407728efacea93589d9608aaec17fe92f
swarupdonepudi <swarup.donepudi@gmail.com>
2023-09-09 18:57:25 -0700

    updated mactl to v13 that has auto detection of dnsmaql installed version

--- Formula/mactl.rb
+++ Formula/mactl.rb
@@ -1,5 +1,5 @@
 class Mactl < Formula
-  version "v0.0.12"
+  version "v0.0.13"
   desc "mactl: cli to quickly bootstrap macbooks."
   homepage "https://github.com/plantoncloud/mactl"
   os_arch = `arch`
