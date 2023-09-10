db4aa1c3d1b21ca837fa31ae7d1ed1b310c6a4c0
swarupdonepudi <swarup.donepudi@gmail.com>
2023-09-09 17:04:31 -0700

    updated mactl to v11 that has gitter installation commented out

--- Formula/mactl.rb
+++ Formula/mactl.rb
@@ -1,5 +1,5 @@
 class Mactl < Formula
-  version "v0.0.10"
+  version "v0.0.11"
   desc "mactl: cli to quickly bootstrap macbooks."
   homepage "https://github.com/plantoncloud/mactl"
   os_arch = `arch`
