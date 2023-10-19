a42f20b24ad9696f325ab847bf8e7695ab9aaea8
swarupdonepudi <swarup.donepudi@gmail.com>
2023-10-19 00:49:01 -0700

    upgrading to v74 that supports displaying progress view for pulumi stack-jobs

--- Formula/planton-cli.rb
+++ Formula/planton-cli.rb
@@ -1,5 +1,5 @@
 class PlantonCli < Formula
-  version "v0.0.73"
+  version "v0.0.74"
   desc "planton-cli: CLI interface to interact with Planton Cloud"
   homepage "https://planton.cloud"
   os_arch = `arch`
