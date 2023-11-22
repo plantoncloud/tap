6e5b6969621b8276b006538fc637d6f0ca6fb766
swarupdonepudi <swarup.donepudi@gmail.com>
2023-11-21 22:47:02 -0800

    upgraded cli to v80 that has environmentInfo changes

--- Formula/planton-cli.rb
+++ Formula/planton-cli.rb
@@ -1,5 +1,5 @@
 class PlantonCli < Formula
-  version "v0.0.77"
+  version "v0.0.80"
   desc "planton-cli: CLI interface to interact with Planton Cloud"
   homepage "https://planton.cloud"
   os_arch = `arch`
