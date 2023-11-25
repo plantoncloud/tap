8b1ba74103e59fd4ef65b78fc069d176e40201ee
swarupdonepudi <swarup.donepudi@gmail.com>
2023-11-25 00:55:26 -0800

    upgrading planton-cli to v83 that has StackJob as Planton Cloud API Resource

--- Formula/planton-cli.rb
+++ Formula/planton-cli.rb
@@ -1,5 +1,5 @@
 class PlantonCli < Formula
-  version "v0.0.82"
+  version "v0.0.83"
   desc "planton-cli: CLI interface to interact with Planton Cloud"
   homepage "https://planton.cloud"
   os_arch = `arch`
