ba04dceb81dd9db52dc48e100c74e914e5152744
swarupdonepudi <swarup.donepudi@gmail.com>
2023-11-24 18:33:55 -0800

    upgrading planton-cli to v82 that displays stack-job log snapshot in table

--- Formula/planton-cli.rb
+++ Formula/planton-cli.rb
@@ -1,5 +1,5 @@
 class PlantonCli < Formula
-  version "v0.0.81"
+  version "v0.0.82"
   desc "planton-cli: CLI interface to interact with Planton Cloud"
   homepage "https://planton.cloud"
   os_arch = `arch`
