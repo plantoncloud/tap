a58e02da0ce8b0ba19e695767c060e20deacfba6
swarupdonepudi <swarup.donepudi@gmail.com>
2023-12-16 04:10:41 -0800

    upgraded to v85 that has resource audit actor updates and version message support

--- Formula/planton-cli.rb
+++ Formula/planton-cli.rb
@@ -1,5 +1,5 @@
 class PlantonCli < Formula
-  version "v0.0.84"
+  version "v0.0.85"
   desc "planton-cli: CLI interface to interact with Planton Cloud"
   homepage "https://planton.cloud"
   os_arch = `arch`
