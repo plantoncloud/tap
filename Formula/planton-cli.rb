17d301d6b2639f08f25efcdbebb90bfc283d2885
swarupdonepudi <swarup.donepudi@gmail.com>
2024-02-03 00:23:10 -0800

    updated planton-cli to v90

--- Formula/planton-cli.rb
+++ Formula/planton-cli.rb
@@ -1,5 +1,5 @@
 class PlantonCli < Formula
-  version "v0.0.88"
+  version "v0.0.90"
   desc "planton-cli: CLI interface to interact with Planton Cloud"
   homepage "https://planton.cloud"
   os_arch = `arch`
