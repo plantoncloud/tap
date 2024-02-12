b5e01415fa26c2a985fb07094f8b623f38c76695
swarupdonepudi <swarup.donepudi@gmail.com>
2024-02-12 01:05:34 -0800

    [skip ci] releasing v95 that has updated snapshot payload with op type in root level

--- Formula/planton-cli.rb
+++ Formula/planton-cli.rb
@@ -1,5 +1,5 @@
 class PlantonCli < Formula
-  version "v0.0.94"
+  version "v0.0.95"
   desc "planton-cli: CLI interface to interact with Planton Cloud"
   homepage "https://planton.cloud"
   os_arch = `arch`
