15d6390475a39e2679cceff480642fbdd36a5413
swarupdonepudi <swarup.donepudi@gmail.com>
2024-02-13 19:43:47 -0800

    [skip ci] releasing v97 that supports get command for sj and st resources using resource-id prefix

--- Formula/planton-cli.rb
+++ Formula/planton-cli.rb
@@ -1,5 +1,5 @@
 class PlantonCli < Formula
-  version "v0.0.96"
+  version "v0.0.97"
   desc "planton-cli: CLI interface to interact with Planton Cloud"
   homepage "https://planton.cloud"
   os_arch = `arch`
