c30208ad3f906b63120a0f249d2364d7ad9f55b2
swarupdonepudi <swarup.donepudi@gmail.com>
2024-02-27 05:37:24 -0800

    [skip ci] updated planton-cli to v103 that supports get, list, update ops on 10 new api resources

--- Formula/planton-cli.rb
+++ Formula/planton-cli.rb
@@ -1,5 +1,5 @@
 class PlantonCli < Formula
-  version "v0.0.102"
+  version "v0.0.103"
   desc "planton-cli: CLI interface to interact with Planton Cloud"
   homepage "https://planton.cloud"
   os_arch = `arch`
