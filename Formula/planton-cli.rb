87ca5c5ad7e3725dd357e0529d2589d4c9e9c276
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-03-11 01:00:12 -0700

    release planton-cli v108 that foreces user to provide message to msi deploy when deploying uncommitted change

--- Formula/planton-cli.rb
+++ Formula/planton-cli.rb
@@ -1,5 +1,5 @@
 class PlantonCli < Formula
-  version "v0.0.107"
+  version "v0.0.108"
   desc "planton-cli: CLI interface to interact with Planton Cloud"
   homepage "https://planton.cloud"
   os_arch = `arch`
