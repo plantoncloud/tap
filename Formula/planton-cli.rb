a2133105fdd4caa619866ae46c553e97d87fa83d
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-06-12 18:28:15 +0530

    release planton-cli v112 that supporting kubernetes operations of listing pods and streaming-logs of api-resources deployed to kubernetes

--- Formula/planton-cli.rb
+++ Formula/planton-cli.rb
@@ -1,5 +1,5 @@
 class PlantonCli < Formula
-  version "v0.0.110"
+  version "v0.0.112"
   desc "planton-cli: CLI interface to interact with Planton Cloud"
   homepage "https://planton.cloud"
   os_arch = `arch`
