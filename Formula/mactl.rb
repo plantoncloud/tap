6ac996999b638ceeada74e2cf584cdae335ac3d9
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-06-14 14:49:16 +0530

    releasing mactl v16 that has b64d and b64e functions

--- Formula/mactl.rb
+++ Formula/mactl.rb
@@ -1,5 +1,5 @@
 class Mactl < Formula
-  version "v0.0.15"
+  version "v0.0.16"
   desc "mactl: cli to quickly bootstrap macbooks."
   homepage "https://github.com/plantoncloud/mactl"
   os_arch = `arch`
