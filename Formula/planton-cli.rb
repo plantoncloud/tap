9fc2bbcbacdfb1c425491a7c84ad356870555512
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-07-12 20:33:04 +0530

    release planton-cli v0.0.115

--- Formula/planton-cli.rb
+++ Formula/planton-cli.rb
@@ -1,5 +1,5 @@
 class PlantonCli < Formula
-  version "v0.0.113"
+  version "v0.0.115"
   desc "planton-cli: CLI interface to interact with Planton Cloud"
   homepage "https://planton.cloud"
   os_arch = `arch`
