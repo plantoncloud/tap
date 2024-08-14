2ca1fe8f224a6e12dba0a6a31de8dcc741c5685e
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-08-14 18:49:04 +0530

    releasing planton-cl v0.0.117 that has planton pulumi up command

--- Formula/planton-cli.rb
+++ Formula/planton-cli.rb
@@ -1,5 +1,5 @@
 class PlantonCli < Formula
-  version "v0.0.115"
+  version "v0.0.117"
   desc "planton-cli: CLI interface to interact with Planton Cloud"
   homepage "https://planton.cloud"
   os_arch = `arch`
