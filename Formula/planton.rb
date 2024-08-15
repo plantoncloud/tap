632f3ce76e4b46de2ea629a74153a5cb7da83483
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-08-15 21:41:57 +0530

    renamed planton-cli to planton and upgraded it to v0.0.118

--- Formula/planton.rb
+++ Formula/planton.rb
@@ -1,14 +1,14 @@
-class PlantonCli < Formula
-  version "v0.0.117"
-  desc "planton-cli: CLI interface to interact with Planton Cloud"
+class Planton < Formula
+  version "v0.0.118"
+  desc "planton: CLI interface for Planton Cloud"
   homepage "https://planton.cloud"
   os_arch = `arch`
   arch = (os_arch.include? "arm64")? "arm64" : "amd64"
-  url "https://storage.googleapis.com/planton-cli/#{version}/planton-cli-#{version}-#{arch}"
+  url "https://storage.googleapis.com/planton-cli/#{version}/planton-#{version}-#{arch}"
   def install
     os_arch = `arch`
     arch = (os_arch.include? "arm64")? "arm64" : "amd64"
-    binary_name="planton-cli-#{version}-#{arch}"
+    binary_name="planton-#{version}-#{arch}"
     bin.install "#{binary_name}"
     mv bin/"#{binary_name}", bin/"planton"
   end
