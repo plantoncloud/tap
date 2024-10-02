9e7fc1d6a295c37e84d48dbd2ec36f31f5a12390
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-10-02 21:35:22 +0530

    added project-planton

---
+++ Formula/project-planton.rb
@@ -0,0 +1,15 @@
+class ProojectPlanton < Formula
+  version "v0.0.1"
+  desc "ProjectPlanton: Multi-Cloud Unified Resource Model (MERM)"
+  homepage "https://project-planton.io"
+  os_arch = `arch`
+  arch = (os_arch.include? "arm64")? "arm64" : "amd64"
+  url "https://storage.googleapis.com/project-planton-downloads/cli/#{version}/project-planton-#{version}-#{arch}"
+  def install
+    os_arch = `arch`
+    arch = (os_arch.include? "arm64")? "arm64" : "amd64"
+    binary_name="project-planton-#{version}-#{arch}"
+    bin.install "#{binary_name}"
+    mv bin/"#{binary_name}", bin/"project-planton"
+  end
+end
