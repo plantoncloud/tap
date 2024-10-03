72271377a35d867f40471cf9ec070bbe8f60f565
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-10-03 19:10:50 +0530

    upgrading project-planton cli to v0.0.9 and removed kube-exec-plugins installations

--- Formula/project-planton.rb
+++ Formula/project-planton.rb
@@ -1,28 +1,15 @@
 class ProjectPlanton < Formula
-  version "v0.0.8"
+  version "v0.0.9"
   desc "ProjectPlanton: Multi-Cloud Unified Resource Model (MERM)"
   homepage "https://project-planton.io"
   os_arch = `arch`
-  arch = (os_arch.include? "arm64") ? "arm64" : "amd64"
+  arch = (os_arch.include? "arm64")? "arm64" : "amd64"
   url "https://storage.googleapis.com/project-planton-downloads/cli/#{version}/project-planton-#{version}-#{arch}"
-  
   def install
     os_arch = `arch`
-    arch = (os_arch.include? "arm64") ? "arm64" : "amd64"
-    binary_name = "project-planton-#{version}-#{arch}"
-    
-    # Install the main CLI binary
-    bin.install binary_name
-    mv bin/binary_name, bin/"project-planton"
-    
-    # Define the kube-client-go plugin URL inside the install method
-    kube_client_go_exec_plugins_version = "v0.0.2"
-    kube_client_gcp_credential_plugin_url = "https://github.com/plantoncloud/kube-client-go-exec-plugins/releases/download/#{kube_client_go_exec_plugins_version}/kube-client-go-gcp-credential-exec-plugin-linux"
-    
-    # Download and install kube-client-go-gcp-exec-plugin
-    system "curl", "-L", kube_client_gcp_credential_plugin_url, "-o", "kube-client-go-gcp-credential-exec-plugin-linux"
-    system "chmod", "+x", "kube-client-go-gcp-credential-exec-plugin-linux"
-    bin.install "kube-client-go-gcp-credential-exec-plugin-linux"
-    mv bin/"kube-client-go-gcp-credential-exec-plugin-linux", bin/"kube-client-go-gcp-exec-plugin"
+    arch = (os_arch.include? "arm64")? "arm64" : "amd64"
+    binary_name="project-planton-#{version}-#{arch}"
+    bin.install "#{binary_name}"
+    mv bin/"#{binary_name}", bin/"project-planton"
   end
 end
