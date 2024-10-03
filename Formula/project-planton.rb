e4295bb0ed05811a925276a78aac3f8974eadf0d
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-10-03 13:28:28 +0530

    release project-planton v0.0.3 in which validate-manifest only validates spec

--- Formula/project-planton.rb
+++ Formula/project-planton.rb
@@ -1,5 +1,5 @@
 class ProjectPlanton < Formula
-  version "v0.0.2"
+  version "v0.0.3"
   desc "ProjectPlanton: Multi-Cloud Unified Resource Model (MERM)"
   homepage "https://project-planton.io"
   os_arch = `arch`
