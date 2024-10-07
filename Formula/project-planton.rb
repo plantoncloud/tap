083220261e1ab3f6a52903a18aebb5dd9ea2197a
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-10-07 21:34:41 +0530

    upgrading project-planton cli to v0.0.11 that supports manifests from url

--- Formula/project-planton.rb
+++ Formula/project-planton.rb
@@ -1,5 +1,5 @@
 class ProjectPlanton < Formula
-  version "v0.0.10"
+  version "v0.0.11"
   desc "ProjectPlanton: Multi-Cloud Unified Resource Model (MERM)"
   homepage "https://project-planton.io"
   os_arch = `arch`
